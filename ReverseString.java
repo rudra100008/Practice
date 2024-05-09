8e79937dcc11b6b964f2b44ecedaa379e1e26dea
Ashum Bhuju Shrestha <136584855+rudra100008@users.noreply.github.com>
2024-05-09 16:07:00 +0545

    Array Practice to reverse the string

--- ReverseString.java
+++ ReverseString.java
@@ -1,17 +1,36 @@
-package com.String;
+package com.Syntax_Array;
+import java.util.Iterator;
+//Write a Java program that takes a string as input and returns a new string with the words 
+//reversed in their order. However, the program should also handle the following edge cases:
+//Empty String or String with Only Spaces: If the input string is empty or consists only of 
+//whitespace characters (spaces, tabs, newlines), the program should return an empty string.
+//Multiple Consecutive Spaces: If the input string contains multiple consecutive spaces, 
+//the program should collapse them into a single space in the reversed output string.
+import java.util.Scanner;
 
 public class ReverseString {
-    public static void main(String[] args) {
-            String s1="Hello Ashum",s2="";
-            char ch;
-            System.out.println("Word:"+ s1);
-            for(int i = s1.length()-1; i>0; --i){
-                ch=s1.charAt(i);
-                s2=s2 + ch;
+	public static String reverseString(String sentence) {
+	    if (sentence.isEmpty()) {
+	        return ""; 
+	    }
 
-            }
-        System.out.println("Word Reversed:"+ s2);
-
-    }
+	    char[] tempChar = new char[sentence.length()];
+	    char[] c= sentence.toCharArray();
+	    for (int i = sentence.length() - 1, j = 0; i >= 0; i--, j++) {
+	        tempChar[j] = c[i];
+	    }
+	    return new String(tempChar); // Return the reversed String object
+	}
+	
+	
+	public static void main(String[] args) {
+//		Scanner input =new Scanner(System.in);
+//		System.out.println("Enter a Sentence:-");
+//		String sentence=input.nextLine();
+		 String sentence="I am Ashum";
+		
+		System.out.println(reverseString( sentence));
+		
+	}
 
 }
